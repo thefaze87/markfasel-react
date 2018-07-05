cb18ae069111a9756d0d648da3aed81ecbdd5206
Mark Fasel <hi@markfasel.com>
2018-07-04 20:07:45 -0500

    update react app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../src/css/app.css';
 import App from '../src/js/App';
 import registerServiceWorker from '../src/js/registerServiceWorker';
 
