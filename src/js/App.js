9a913d56894a559ed1308cdafe06f935c69b66e9
Mark Fasel <markfasel@gmail.com>
2019-01-09 15:31:17 -0600

    Update content

--- src/js/App.js
+++ src/js/App.js
@@ -1,33 +1,34 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  render() {
-    return (
-      <div class="container">
-        <div class="row">
-          <header class="masthead col-md-12">
-            <div class="inner">
-              <h1 class="masthead-brand"><img src="images/logo-new.svg" /></h1>
+    render() {
+        return (
+
+            <div class="container">
+                <div class="row">
+                    <header class="masthead col-md-12">
+                        <div class="inner">
+                            <h1 class="masthead-brand"><img src="images/logo-new.svg" /></h1>
+                        </div>
+                    </header>
+                </div>
+                <div class="row">
+                    <main role="main" class="col-md-10">
+                        <h2 class="name">Hey, I'm Mark Fasel</h2>
+                        <p class="lead">A digital marketing consultant with a huge passion for interactive &amp; UX design, full stack development &amp; digital strategy. Currently a Software Developer at <a href="https://www.differential.com" target="_blank">Differential</a> working remotely from the Greater Nashville Tennessee area. Former Front-end Developer at <a href="https://www.daveramsey.com" target="_blank">Dave Ramsey</a> and Former Web Application Developer at <a href="https://www.hopkinsallchildrens.org" target="_blank">Johns Hopkins All Children’s Hospital</a>.</p>
+                        <p>Fourteen years experience working with top industry brands and building digital products. <em>Currently taking on new projects</em>. Current development stack is <strong>PHP, Rails, and Java</strong> for backend development and <strong>HTML5, CSS3, JavaScript (ReactJs, Vue, and jQuery)</strong> for front end development. I am proficient in Git and version control, SQL architecture and development and full stack solutions.</p>
+                        <p>See my recent work on <a href="http://www.dribbble.com/MarkFasel" target="_blank">Dribbble</a>, ramblings on <a href="http://www.twitter.com/MarkFasel" target="_blank">Twitter</a>, socialize on <a href="https://www.facebook.com/markfaselconsulting/" target="_blank">Facebook</a> read my latest thoughts on <a href="https://medium.com/@markfasel" target="_blank">Medium</a> and full work history on <a href="http://linkedin.com/in/markfasel/" target="_blank">Linkedin</a>. For new work inquiries, questions &amp; comments, speaking engagements, or if you just want to say hi, <a href="mailto:hi@markfasel.com?subject=New%20Message%20from%20www.MarkFasel.com&body=">drop me a message</a>.</p>
+                    </main>
+                </div>
+                <div class="row">
+                    <footer class="mastfoot col-md-12">
+                        <p>Copyright &copy; 2018 - Mark Fasel</p>
+                        <p>All rights reserved.</p>
+                    </footer>
+                </div>
             </div>
-          </header>
-        </div>
-        <div class="row">
-          <main role="main" class="col-md-10">
-        <h2 class="name">Hey, I'm Mark Fasel</h2> 
-            <p class="lead">A digital marketing consultant with a huge passion for interactive &amp; UX design, full stack development &amp; digital strategy. Currently a Front End Developer at <a href="https://www.daveramsey.com" target="_blank">Ramsey Solutions</a> in Greater Nashville Tennessee. Former Web Application Developer at <a href="https://www.hopkinsallchildrens.org" target="_blank">Johns Hopkins Medicine All Children’s Hospital</a>.</p>
-            <p>Fourteen years experience working with top industry brands and building digital products. <em>Currently taking on new projects</em>. Current development stack is <strong>PHP, Rails, and Java</strong> for backend development and <strong>HTML5, CSS3, JavaScript (ReactJs, Vue, and jQuery)</strong> for front end development. I am proficient in Git and version control, SQL architecture and development and full stack solutions.</p>
-            <p>See my recent work on <a href="http://www.dribbble.com/MarkFasel" target="_blank">Dribbble</a>, ramblings on <a href="http://www.twitter.com/MarkFasel" target="_blank">Twitter</a>, socialize on <a href="https://www.facebook.com/markfaselconsulting/" target="_blank">Facebook</a> read my latest thoughts on <a href="https://medium.com/@markfasel" target="_blank">Medium</a> and full work history on <a href="http://linkedin.com/in/markfasel/" target="_blank">Linkedin</a>. For new work inquiries, questions &amp; comments, speaking engagements, or if you just want to say hi, <a href="mailto:hi@markfasel.com?subject=New%20Message%20from%20www.MarkFasel.com&body=">drop me a message</a>.</p>
-          </main>
-      </div>
-      <div class="row">
-        <footer class="mastfoot col-md-12">
-              <p>Copyright &copy; 2018 - Mark Fasel</p>
-              <p>All rights reserved.</p>
-            </footer>
-      </div>
-    </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
